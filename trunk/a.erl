8c08b9df3464505d3e1d28a3ee517f1d65190969
Gabriel.Tholsgard@gmail.com <Gabriel.Tholsgard@gmail.com@4b514c45-add5-b4ef-5f78-1fb934a3fe87>
2011-05-11 22:41:40 +0000

    Updating

---
+++ trunk/a.erl
@@ -0,0 +1,27 @@
+-module(a).
+-compile(export_all).
+
+client() ->
+    SomeHostInNet = {192,168,0,103},
+    {ok, Sock} = gen_tcp:connect(SomeHostInNet, 8080,[binary]),
+    io:format("Sock:  ~p = ~p\n", [Sock, inet:sockname(Sock)]),
+    ok = gen_tcp:send(Sock, "Some Data"),
+    ok = gen_tcp:close(Sock).
+
+
+server() ->
+    {ok, LSock} = gen_tcp:listen(8080, [binary]),
+    io:format("LSock: ~p = ~p\n", [LSock, inet:sockname(LSock)]),
+    {ok, Sock} = gen_tcp:accept(LSock),
+    io:format("Sock:  ~p = ~p\n", [Sock, inet:sockname(Sock)]),
+    {_, Bin} = do_recv(Sock, []),
+    ok = gen_tcp:close(Sock),
+    Bin.
+
+do_recv(Sock, Bs) ->
+    case gen_tcp:recv(Sock, 0) of
+        {ok, B} ->
+            do_recv(Sock, [Bs, B]);
+        {error, _} ->
+            {ok, list_to_binary(Bs)}
+    end.
