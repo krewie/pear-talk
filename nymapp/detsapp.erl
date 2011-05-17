d772661def6d2c4c69bbd4d0949e88305b3a0b2e
martenblomberg@gmail.com <martenblomberg@gmail.com@4b514c45-add5-b4ef-5f78-1fb934a3fe87>
2011-05-17 10:05:39 +0000

    la in server.erl i ny mapp

---
+++ nymapp/detsapp.erl
@@ -0,0 +1,104 @@
+%% @author Göran Hagelin, Gabriel Tholsgård
+%% @doc modul som innehåller DETS funktioner.
+ 
+-module(detsapp).
+-export([openTable/2, closeTable/1, add/3,isDets/1, delete/2, retrieve/2, sync/1, open/1]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%% @doc openTable funktion som öppnar ett DETS table eller skapar en ny fil om det behövs. Args är en lista med valmöjligheter när man öppnar ett table, Args kan vara [] för default värden. 
+%% @spec atom * {Key, Value} list ->  {ok, TableName} | {error, Reason}
+
+openTable(FileName, Args) ->
+    dets:open_file(FileName, Args).
+
+%% @doc closeTable funktion som stänger en öppen table och skriver alla ändringar till filen på disk.
+%% @spec TableName -> ok | {error, Reason}
+closeTable(Table) ->
+    dets:close(Table).
+
+%% @doc add funktion som lägger till en tupel {Key, Value} (objekt hädanefter) i Table.
+%% @spec TableName, String, Value | [Values] -> ok | {error, Reason}
+add(Table,Key, Value) ->
+    dets:insert(Table, {Key, Value}).
+
+%% @doc isDets funktion som returnerar true om argumentet är ett DETS table.
+%% @spec atom -> true | {errorl, Reason}
+isDets(FileName) ->
+    dets:is_dets_file(FileName).
+
+%% @doc delete funktion som tar bort ett objekt ur Table med nyckeln Key.
+%% @spec TableName * String -> ok | {error, Reason}
+delete(Table, Key) ->
+    dets:delete(Table, Key).
+
+%% @doc sync funktion som skriver alla ändringar i Table till filen på disk.
+%% @spec TableName -> ok | {error, Reason}
+sync(Table) ->
+    dets:sync(Table).
+
+%% @doc retrieve funktion som hämtar en lista av objekt från Table med nyckeln Key. Om inga object hittades returneras tom lista.
+%% @spec TableName * String -> [{String, Value | [Values]}] | {error, Reason}
+retrieve(Table, Key) ->
+    dets:lookup(Table, Key).
+
+%% @doc open funktion som öppnar ett DETS table om filen FileName är ett DETS table (kan ej skapa nya filer vilket openTable kan). 
+%% @spec atom -> {ok, TableName} | {error, Reason}
+open(FileName) ->
+    case isDets(FileName) of
+	true ->
+	    dets:open_file(FileName);
+	{error, Reason} -> {error, Reason}
+    end.
+	      
+
+%% eunit tests
+
+openTable_test_() ->
+    File = openTable(detsapptable.test,[]),
+    {_ , Cake} = File,
+    [
+     ?_assertEqual({ok, Cake}, File),
+     ?_assertEqual( ok, closeTable(Cake))
+    ].
+
+closeTable_test_() ->
+    {_, Cake} = openTable(detsapptable.test, []),
+    [
+     ?_assertEqual( ok, closeTable(Cake))
+    ].
+
+add_test_() ->
+    {ok, Cake} = openTable(detsapptable.test,[]),
+    [
+     ?_assertEqual(ok, add(Cake, "Key", ["smurfs"])),
+     ?_assertEqual( ok, closeTable(Cake))
+    ].
+
+isDets_test()->
+    ?assertEqual(true, isDets(detsapptable.test)).
+
+retrieve_test_()->
+    {ok, Cake} = openTable(detsapptable.test,[]),
+    [
+     ?_assertEqual([{"Key", ["smurfs"]}], retrieve(Cake, "Key")),
+     ?_assertEqual( ok, closeTable(Cake))
+    ].
+
+
+sync_test_()->
+    {ok, Cake} = openTable(detsapptable.test,[]),
+    [
+     ?_assertEqual(ok, sync(Cake)), 
+     ?_assertEqual( ok, closeTable(Cake))
+    ].
+
+
+delete_test_() ->
+    {ok, Cake} = openTable(detsapptable.test,[]),
+    [
+     ?_assertEqual(ok, delete(Cake, "Key")),
+     ?_assertMatch ([], retrieve(Cake, "Key")),
+     ?_assertEqual( ok, closeTable(Cake))
+    ].
+
--- nymapp/server.erl
+++ nymapp/server.erl
@@ -1,27 +1,28 @@
 -module(server).
 -compile(export_all).
--define(TCP_OPTIONS, [binary]).
+-define(TCP_OPTIONS, [binary, {active, true}]).
 -define(PORT, 8888).
 -define(TIMEOUT, 12000).
 
 %Server calls
 % ------- funktioner att implementera för servernätverk
 
-join_network(server) ->
+%join_network(server) ->
 
 % --------
 
 start_server() ->
 	 {ok, ListenSock} = gen_tcp:listen(?PORT, ?TCP_OPTIONS),
-	 DBpid = spawn(serv_ul, start, [self()])
+	 DBpid = spawn(serv_ul, start, []),
 	 wait_for_connection(ListenSock, DBpid).
 	 
 wait_for_connection(ListenSocket, DBpid) ->
-	 case gen_tcp:accept(ListenSock) of
+	 case gen_tcp:accept(ListenSocket) of
 	 	% när någon connectar spawnar vi en process som hanterar meddelanden från den socketen,
 	 	% samtidigt som vi loopar och väntar på fler connections.
-	 	{ok, Socket} -> spawn(?MODULE, listen_state, [Socket, DBpid]),
-	 					wait_for_connection(ListenSocket, DBpid);
+	 	{ok, Socket} -> spawn(?MODULE, wait_for_connection, [ListenSocket, DBpid]),
+	 					io:format("spawned process~n"),
+	 					listen_state(Socket, DBpid);
 		_ -> ok
 	end.
 
@@ -29,28 +30,33 @@ wait_for_connection(ListenSocket, DBpid) ->
 listen_state(Socket, DBPid) ->
 	receive
 		{tcp, Socket, Packet} ->
+			io:format("recieved request~n"),
 			case binary_to_term(Packet) of
 		%Client-calls
-				{client, login, ID} ->
-					{ok, Address, Port} = inet:peername(Socket),
-					DBPid ! {client, login, ID, [Address, Port], self()};
+				{client, login, ID, Port} ->
+					{ok, {Address, _}} = inet:peername(Socket),
+					DBPid ! {client, login, ID, Address, Port, self()},
+					listen_state(Socket, DBPid);
 				{client, addfriend, {MyID, FriendID}} ->
 					DBPid ! {client, addfriend, {MyID, FriendID}, self()};
 				{client, removefriend, {MyID, FriendID}} ->
 					DBPid ! {client, removefriend, {MyID, FriendID}, self()};
 				{client, changename, ID, Name} ->
-					DBPid ! {client, changename, ID, Name, self()};
+					DBPid ! {client, changename, ID, Name, self()}
 		%Server-calls
 				%{server, serverlogin, {Netinfo}} ->
 					%se till så att servern blir up-to-date
-				%{server, userinfo, {Server, Client, Netinfo}} ->
+				%{server, userinfo, {server, client, Netinfo}} ->
 					%skicka tillbaka clientinfo till server som ännu inte har infon
 					%funkar för clientlogin likaså när man skapar vänlistan
 				%{server, 
 				
-			end
+			end;
+			
 		{db, friendlist, Friendlist} ->
-				gen_tcp:send(Socket, {server, friendlist, Friendlist});
+				io:format("sending friendlist: ~w to ~w~n", [Friendlist, Socket]),
+				Data = term_to_binary({[], [], [], [], [], friendlist, [], [], Friendlist}),
+				send(Socket, Data)
 		%{db, addfriend, ok} ->
 			%friend succesfully added
 		%{db, removefriend, ok} ->
@@ -58,8 +64,14 @@ listen_state(Socket, DBPid) ->
 		%{db, changename, ok} ->
 			%user succesfully changed name
 			
-	after ?TIMEOUT ->
-			gen_tcp:close(Socket)	
-	end
+	%after ?TIMEOUT ->
+	%		gen_tcp:close(Socket)	
+	end,
 	listen_state(Socket, DBPid).
+	
+	send(Socket, <<Chunk:100/binary, Rest/binary>>) ->
+		gen_tcp:send(Socket, Chunk),
+		send(Socket, Rest);
+	send(Socket, Rest) ->
+		gen_tcp:send(Socket, Rest).
 	
