1d8b6c74405476e520c25327a931b4aee96ab2dd
martenblomberg@gmail.com <martenblomberg@gmail.com@4b514c45-add5-b4ef-5f78-1fb934a3fe87>
2011-05-18 12:16:54 +0000

    added the server

--- trunk/server.erl
+++ trunk/server.erl
@@ -1,35 +1,82 @@
 -module(server).
 -compile(export_all).
+-define(TCP_OPTIONS, [binary, {active, true}]).
+-define(PORT, 8888).
+-define(TIMEOUT, 12000).
 
-start() ->
-	{ok, Sock} = gen_udp:open(8888, [binary,{active, true}]),
-	listen(Sock, []).
+%Server calls
+% ------- funktioner att implementera för servernätverk
+
+%join_network(server) ->
+
+% --------
+
+start_server() ->
+	 {ok, ListenSock} = gen_tcp:listen(?PORT, ?TCP_OPTIONS),
+	 DBpid = spawn(serv_ul, start, []),
+	 wait_for_connection(ListenSock, DBpid).
+	 
+wait_for_connection(ListenSocket, DBpid) ->
+	 case gen_tcp:accept(ListenSocket) of
+	 	% när någon connectar spawnar vi en process som hanterar meddelanden från den socketen,
+	 	% samtidigt som vi loopar och väntar på fler connections.
+	 	{ok, Socket} -> spawn(?MODULE, wait_for_connection, [ListenSocket, DBpid]),
+	 					io:format("spawned process~n"),
+	 					listen_state(Socket, DBpid);
+		_ -> ok
+	end.
 
 
-listen(Sock, [])->
-   receive
-   	{udp, Socket, IP, InPortNo, Packet} ->
-   		io:format("recieved first client: ~p Port:~p\n", [IP, InPortNo]),
-   		case inet:sockname(Socket) of
-   			{ok, {Address, Port}} ->
-   				io:format("Socket is Add: ~p Port: ~p\n", [Address, Port]);
-   			{error, _} ->
-   				io:format("Some error\n", [])
-   		end,
-   		listen(Sock, [IP])
-    end;
-
-listen(Sock, [H|T])->
-   receive
-   	{udp, Socket, IP, InPortNo, Packet} ->
-	   	io:format("recieved second client: ~p Port:~p\n", [IP, InPortNo]),
-	   	case inet:sockname(Socket) of
-   			{ok, {Address, Port}} ->
-   				io:format("Socket is Add: ~p Port: ~p\n", [Address, Port]);
-   			{error, _} ->
-   				io:format("Some error\n", [])
-   		end,
-   		gen_udp:send(Sock, IP, 8888, term_to_binary([H, IP])),
-	   	io:format("sent message to second client: ~p\n", [H]),
-   		listen(Sock, [IP]++[H|T])
-    end.
+listen_state(Socket, DBPid) ->
+	receive
+		{tcp, Socket, Packet} ->
+			io:format("recieved request~n"),
+			case binary_to_term(Packet) of
+		%Client-calls
+				{client, login, ID, Port} ->
+					{ok, {Address, _}} = inet:peername(Socket),
+					DBPid ! {client, login, ID, Showedname, [Address, Port], Password, self()},
+					listen_state(Socket, DBPid);
+				{client, addfriend, {MyID, FriendID}} ->
+					DBPid ! {client, addfriend, {MyID, FriendID}, self()};
+				{client, removefriend, {MyID, FriendID}} ->
+					DBPid ! {client, removefriend, {MyID, FriendID}, self()};
+				{client, changename, ID, Name} ->
+					DBPid ! {client, changename, ID, Name, self()}
+		%Server-calls
+				%{server, serverlogin, {Netinfo}} ->
+					%se till så att servern blir up-to-date
+				%{server, userinfo, {server, client, Netinfo}} ->
+					%skicka tillbaka clientinfo till server som ännu inte har infon
+					%funkar för clientlogin likaså när man skapar vänlistan
+				%{server, 
+				
+			end;
+			
+		{db, friendlist, Friendlist} ->
+				io:format("sending friendlist: ~w to ~w~n", [Friendlist, Socket]),
+				Data = term_to_binary({[], [], [], [], [], friendlist, [], [], Friendlist}),
+				send(Socket, Data)
+		{db, badPass} ->
+		
+		{db, badID} ->
+		
+		
+		%{db, addfriend, ok} ->
+			%friend succesfully added
+		%{db, removefriend, ok} ->
+			%friend succesfully removed
+		%{db, changename, ok} ->
+			%user succesfully changed name
+			
+	%after ?TIMEOUT ->
+	%		gen_tcp:close(Socket)	
+	end,
+	listen_state(Socket, DBPid).
+	
+	send(Socket, <<Chunk:100/binary, Rest/binary>>) ->
+		gen_tcp:send(Socket, Chunk),
+		send(Socket, Rest);
+	send(Socket, Rest) ->
+		gen_tcp:send(Socket, Rest).
+	
